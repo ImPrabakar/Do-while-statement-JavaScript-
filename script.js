38d82bca56adb3bba256c9efd499ca462003a7fb
Prabàkár <55774504+ImPrabakar@users.noreply.github.com>
2019-09-25 19:51:51 +0530

    `<script>` stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+num=1
+do
+{
+document.write("You Are on the do while statement")
+document.write("<BR>")
+document.write("The value of number is:"+num)
+num++
+document.write("<BR>")
+document.write("<BR>")
+}
+while(num<=3)
+
+document.write("<BR>")
+document.write("You Are Now outside the do while statement")
